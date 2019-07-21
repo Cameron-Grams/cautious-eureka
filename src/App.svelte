ae106274654bf4fcd7edc76756af779d6d61c1de
Cameron <cameron.grams.2@gmail.com>
2019-07-21 08:58:30 -0400

    cleaned up the examples

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script>
     import Second from './Second.svelte'
     import Arrays from './Arrays.svelte'
+    import Third from './third.svelte'
 	export let name, second; 
+
+    let hideMe = false;
+
+    function toggle2(){
+        hideMe = !hideMe;
+    }
+
 </script>
 
 <style>
@@ -10,7 +18,26 @@
 	}
 </style>
 
+
+
 <h1>This is {name}!</h1>
+
 <Second second={second}/>
+
 <Arrays/>
-<Second/>
+
+{#if hideMe}
+    <Second/>
+{/if}
+
+<div>
+<button on:click={toggle2}>
+    {#if !hideMe}
+        Another Second...
+    {:else}
+        Lose the Second Second...
+    {/if}
+</button>
+</div>
+
+<Third/>
---
+++ src/third.svelte
@@ -0,0 +1,42 @@
+<script>
+    let shower = false;
+    let counter = 0;
+
+function addCounter(){
+    counter += 1;
+    }
+
+    function toggle(){
+        shower = !shower;
+    }
+
+</script>
+
+<div>
+
+
+<p>{counter}</p>
+
+<button on:click={addCounter}>
+    Add On!
+</button>
+
+{#if counter<5}
+        <p>Hello There...</p>
+    {:else if counter == 5 }
+        <p>BOOM!</p>
+    {:else}
+        <p>Still rambling...</p>
+{/if}
+
+
+{#if shower}
+    <button on:click={toggle}>
+        Old
+    </button>
+{:else }
+    <button on:click={toggle}>
+        New
+    </button>
+{/if}
+</div>
